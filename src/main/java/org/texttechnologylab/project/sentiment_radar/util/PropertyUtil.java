3401668878e404591917bc78311522ddb4413b04
s3391912@stud.uni-frankfurt.de <Gitbzl1#>
2022-02-09 10:11:45 +0100

    Added Java doc for PropertyUtil

--- src/main/java/org/texttechnologylab/project/sentiment_radar/util/PropertyUtil.java
+++ src/main/java/org/texttechnologylab/project/sentiment_radar/util/PropertyUtil.java
@@ -5,13 +5,25 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PropertyUtil {
+  /**
+   * This class contains functions to load properties for a mongoDB connection.
+   * @author Philipp Daechert
+   * @throws IOException
+   */
   private static Properties loadProperties() throws IOException {
+    /**
+     * Function to load an app.properties file to extract connection information.
+     */
     Properties appProps = new Properties();
     appProps.load(PropertyUtil.class.getClassLoader().getResourceAsStream("app.properties"));
     return appProps;
   }
 
   public static String getRemoteHost() {
+    /**
+     * Function to get the "remote_host" information of the Property Class.
+     * @throws IOException
+     */
     try {
       return loadProperties().getProperty("remote_host");
     } catch (IOException e) {
@@ -21,6 +33,10 @@ public class PropertyUtil {
   }
 
   public static String getRemoteDatabase() {
+    /**
+     * Function to get the "remote_database" information of the Property Class.
+     * @throws IOException
+     */
     try {
       return loadProperties().getProperty("remote_database");
     } catch (IOException e) {
@@ -30,6 +46,10 @@ public class PropertyUtil {
   }
 
   public static String getRemoteUser() {
+    /**
+     * Function to get the "remote_user" information of the Property Class.
+     * @throws IOException
+     */
     try {
       return loadProperties().getProperty("remote_user");
     } catch (IOException e) {
@@ -39,6 +59,10 @@ public class PropertyUtil {
   }
 
   public static String getRemotePassword() {
+    /**
+     * Function to get the "remote_password" information of the Property Class.
+     * @throws IOException
+     */
     try {
       return loadProperties().getProperty("remote_password");
     } catch (IOException e) {
@@ -48,6 +72,10 @@ public class PropertyUtil {
   }
 
   public static String getRemotePort() {
+    /**
+     * Function to get the "remote_port" information of the Property Class.
+     * @throws IOException
+     */
     try {
       return loadProperties().getProperty("remote_port");
     } catch (IOException e) {
@@ -57,6 +85,10 @@ public class PropertyUtil {
   }
 
   public static String getRemoteCollection() {
+    /**
+     * Function to get the "remote_collection" information of the Property Class.
+     * @throws IOException
+     */
     try {
       return loadProperties().getProperty("remote_collection");
     } catch (IOException e) {
